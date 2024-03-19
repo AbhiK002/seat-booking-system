2e5501a172fc34bf59c59439afd53d298e050711
MohammadAliHassan000 <105143340+MohammadAliHassan000@users.noreply.github.com>
2024-03-19 10:23:54 +0530

    fixed displayDetails bug

--- src/main/java/com/project/sbs/database/repositories/SeatRepository.java
+++ src/main/java/com/project/sbs/database/repositories/SeatRepository.java
@@ -4,12 +4,14 @@ import com.project.sbs.database.entities.Booking;
 import com.project.sbs.database.entities.Floor;
 import com.project.sbs.database.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, Integer> {
     List<Seat> getSeatsBySeatFloorId(Floor seatFloorId);
 
+    @Query("select f from Seat f where f.seatFloorId= :floor ")
     List<Booking> getAllSeatsByFloorId(Floor floor);
 
 }
