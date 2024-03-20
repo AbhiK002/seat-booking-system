0c320bd8c2ff85974f738c66a1e9b1cd530cf21a
Abhineet Kelley <abhineet.kelley@gmail.com>
2024-03-20 09:54:57 +0530

    Testing 1

--- src/test/java/com/project/sbs/SeatBookingSystemApplicationTests.java
+++ src/test/java/com/project/sbs/SeatBookingSystemApplicationTests.java
@@ -1,13 +1,35 @@
 package com.project.sbs;
 
+import com.project.sbs.api.controllers.auth.AuthController;
+import com.project.sbs.api.services.auth.AuthService;
+import com.project.sbs.database.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SeatBookingSystemApplicationTests {
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private AuthService authService;
+
+    @InjectMocks
+    private AuthController authController;
+
     @Test
-    void contextLoads() {
+    public void authController(){
+        when(authService.userAlreadyExists("hello.com"))
+                .thenReturn(false);
+        boolean actualReturn = false;
+        assertFalse(actualReturn);
     }
 
 }
